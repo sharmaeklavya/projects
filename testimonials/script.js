88142d6121cf16908364c0c34d9fd31f7ce92a8c
sharmaeklavya <24450132+sharmaeklavya@users.noreply.github.com>
2020-08-17 19:37:44 +0530

    Update script.js

--- testimonials/script.js
+++ testimonials/script.js
@@ -9,13 +9,25 @@ first.addEventListener("click", slider);
 second.addEventListener("click", slider);
 
 function slider() {
-  if (active) {
-    visible.style.display = "none";
-    hidden.style.display = "grid";
-    active = false;
+   if (screen.width < 375) {
+    if (active) {
+      visible.style.display = "none";
+      hidden.style.display = "flex";
+      active = false;
+    } else {
+      visible.style.display = "flex";
+      hidden.style.display = "none";
+      active = true;
+    }
   } else {
-    visible.style.display = "grid";
-    hidden.style.display = "none";
-    active = true;
+    if (active) {
+      visible.style.display = "none";
+      hidden.style.display = "grid";
+      active = false;
+    } else {
+      visible.style.display = "grid";
+      hidden.style.display = "none";
+      active = true;
+    }
   }
 }
