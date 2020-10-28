a1a035c196e4b51ea42d93998af8f2d7dd72d590
sharmaeklavya <24450132+sharmaeklavya@users.noreply.github.com>
2020-10-29 00:59:17 +0530

    updated handbook refined search

--- handbook/js/link/link.js
+++ handbook/js/link/link.js
@@ -15,7 +15,7 @@ const choice = document.getElementById('choice')
 
 // function default content as page loads
 const home = () => tfspeech.classList.add('show-item');
-displayFaculty()
+displayFaculty(facultyList)
 displayContent(pound)
 
 // Event Listner for navigating link tabs
@@ -47,8 +47,8 @@ function displayContent (input){
 }
 
 // function to display faculty list
-function displayFaculty(){
-const details = facultyList.map(tf => {
+function displayFaculty(faculty){
+const details = faculty.map(tf => {
     return `   
     <div class="faculty">
         <div class="img">
@@ -67,57 +67,34 @@ const details = facultyList.map(tf => {
     facultyData.innerHTML = `${details}`
 }
 
-
-
+// form event listener for search
 form.addEventListener('input', (e) => {
     e.preventDefault();
     const searchValue = e.target.value.toLowerCase()
     const choiceValue = choice.value
-   
-   if (searchValue){    
-       const display = filterValue.map(tf => {
-           return `  <div class="faculty">
-           <div class="img">
-               <img src="images/avatar.jpg" alt="">
-           </div>
-           <div class="disc">
-               <span class="sub-title">${tf.name}<div class="status">${tf.status}</div></span>
-               <span class="para">Add: ${tf.address}</span>
-               <span class="para">Phone: ${tf.cell}</span>           
-               <span class="para" style="word-break: break-all">Email: ${tf.email}</span>
-               <span class="para">Inst: ${tf.institution}</span>            
-               <span class="para">H Studio: ${tf.homestudio} | Location ID: ${tf.locationid}</span>             
-           </div>
-       </div>`
-       }).join('')
-
-       facultyData.innerHTML = display
-
-   } else{
-    displayFaculty()
-   }
-
-   const filterValue = facultyList.filter(fl => {
-    if (searchValue){
-        if (choiceValue == 'all'){
-            const value = [fl.name, fl.address, fl.cell, fl.email, fl.institution, fl.homestudio, fl.locationid].toString().toLowerCase()
-            return value.includes(searchValue)
+    if (searchValue === '') return displayFaculty(facultyList)
+    const filterValue = facultyList.filter(fl => {         
+       if (searchValue) {
+            if (choiceValue == 'all' || choiceValue == 'none'){            
+                const value = [fl.name, fl.address, fl.cell, fl.email, fl.institution, fl.homestudio, fl.locationid].toString().toLowerCase()
+                return value.includes(searchValue)
+            }
+            else if (choiceValue == 'name'){
+                const value = [fl.name].toString().toLowerCase()
+                return value.includes(searchValue)
+            }
+            else if (choiceValue === 'homestudio'){
+                const value = [fl.homestudio].toString().toLowerCase()
+                return value.includes(searchValue)
+            }
+            else if (choiceValue === 'phone'){
+                const value = [fl.cell].toString().toLowerCase()
+                return value.includes(searchValue)
+            } else if (choiceValue === 'email'){
+                const value = [fl.email].toString().toLowerCase()
+                return value.includes(searchValue)
+            }
         }
-        else if (choiceValue == 'name'){
-            const value = [fl.name].toString().toLowerCase()
-            return value.includes(searchValue)
-        }
-        else if (choiceValue === 'homestudio'){
-            const value = [fl.homestudio].toString().toLowerCase()
-            return value.includes(searchValue)
-        }
-        else if (choiceValue === 'phone'){
-            const value = [fl.cell].toString().toLowerCase()
-            return value.includes(searchValue)
-        } else if (choiceValue === 'email'){
-            const value = [fl.email].toString().toLowerCase()
-            return value.includes(searchValue)
-        }        
-    }
-})
+    })
+    displayFaculty(filterValue)
 })
